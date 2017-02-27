2dff9319fddadfef6455905239b4dd6910f1c55a
KongFanyi <kelefy@qq.com>
2017-02-27 18:50:17 +0800

    Initial Commit

---
+++ OpenTheDoor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OpenTheDoor
+//
+//  Created by KongFanyi on 2017/2/27.
+//  Copyright © 2017年 KongFanyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
